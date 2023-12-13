a51d9eae67b2f7b4c669f380ec3ec397d3f00eda
student_ait409 <prostoi.cchelovek@gmail.com>
2023-12-13 15:37:17 +0300

    Имплементация поведения Cool Timer Часть 3

--- app/src/main/java/com/example/sharedpreference/MainActivity.java
+++ app/src/main/java/com/example/sharedpreference/MainActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private SeekBar seekBar;
     private TextView textView;
+    private boolean isTimerOn;
+    private Button button;
+    private CountDownTimer countDownTimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +27,9 @@ public class MainActivity extends AppCompatActivity {
         seekBar = findViewById(R.id.seekBar);
         textView = findViewById(R.id.textView);
         seekBar.setMax(600);
-        seekBar.setProgress(60);
+        seekBar.setProgress(30);
+        isTimerOn=false;
+        button = findViewById(R.id.button);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
@@ -43,17 +50,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void start(View view) {
-        CountDownTimer countDownTimer = new CountDownTimer(seekBar.getProgress()*1000,1000) {
-            @Override
-            public void onTick(long l) {
-                updateTime(l);
-            }
+        if (isTimerOn){
+            button.setText("Stop");
+            seekBar.setEnabled(false);
+            isTimerOn=true;
+            countDownTimer = new CountDownTimer(seekBar.getProgress()*1000,1000) {
+                @Override
+                public void onTick(long l) {
+                    updateTime(l);
+                }
 
-            @Override
-            public void onFinish() {
-                Log.d("onFinish:","Finish!");
-            }
-        }.start();
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.bell_sound);
+                    mediaPlayer.start();
+                    resetTimer();
+                }
+            };
+            countDownTimer.start();
+        } else{
+resetTimer();
+        }
+
     }
     private void updateTime(long l){
         int minuts = (int)l/1000/60;
@@ -65,5 +83,13 @@ public class MainActivity extends AppCompatActivity {
         if (seconds<10){secondsString = "0"+seconds;}
         else {secondsString = String.valueOf(seconds);}
         textView.setText(minutesString + ":"+secondsString);
+    }
+    private void resetTimer(){
+        countDownTimer.cancel();
+        textView.setText("00:30");
+        button.setText("start");
+        seekBar.setEnabled(true);
+        seekBar.setProgress(60);
+        isTimerOn=false;
     }
 }
